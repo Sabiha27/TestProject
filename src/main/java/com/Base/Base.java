8954d8136e378e1238e129f36f7f697bbd6ed9a4
sabiha <sabihasubhedar@gmail.com>
2020-03-06 20:26:40 +0530

    updated commit

---
+++ src/main/java/com/Base/Base.java
@@ -0,0 +1,34 @@
+package com.Base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class Base {
+	public static WebDriver driver;
+	public static Properties prop;
+	
+	public Base(){
+		try {
+			prop = new Properties();
+			FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/Config/config.properties");
+			
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+	}
+	
+
+
+	
+	
+
+}
